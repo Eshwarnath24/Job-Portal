44b172f1be99b6f14df02f790e1948af3c5ee8bc
Eshwarnath24 <gajulaeshwarnath24@gmail.com>
2025-12-13 19:34:06 +0530

    deploy and update errors (still not cleared)

--- server/config/db.js
+++ server/config/db.js
@@ -1,10 +1,11 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
-  mongoose.connection.on('connected', () => console.log('Database connected'));
+  mongoose.connection.on("connected", () => console.log("Database connected"));
 
-  await mongoose.connect(`${process.env.MONGODB_URI}/job-portal`)
-    
+  await mongoose.connect(process.env.MONGODB_URI, {
+    dbName: "job-portal",
+  });
 };
 
 export default connectDB;
